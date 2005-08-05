bee52f148865898f03af5b626f1168e5c502a1d8
umeboshi <umeboshi@cfc4e7be-4be4-0310-bcfe-fc894edce94f>
2005-08-05 18:15:44 +0000

    updating index.php

--- website/index.php
+++ website/index.php
@@ -7,38 +7,6 @@
 </head>
 <body>
 
-<!-- top strip -->
-<table BORDER=0 CELLSPACING=0 CELLPADDING=2 WIDTH="100%" BGCOLOR="#7B7B7B" >
-<tr>
-<td><span class=maintitlebar>&nbsp; 
-<b><a href="http://www.berlios.de/index.php.en" class="maintitlebar">Home</a></b> |
-<b><a href="http://developer.berlios.de/about.php" class="maintitlebar">About us</a></b> |
-<b><a href="http://developer.berlios.de/partners.php" class="maintitlebar">Partners</a></b> |
-<b><a href="http://developer.berlios.de/contact.php" class="maintitlebar">Contact</a></b></span></td>
-</tr>
-</table>
-<!-- end top strip -->
-
-<!-- top title -->
-<table BORDER=0 CELLSPACING=0 CELLPADDING=0 WIDTH="100%" >
-<tr VALIGN=TOP BGCOLOR="#FFCC33">
-<td BGCOLOR="#FFFFFF"><a href="http://www.berlios.de/index.php.en"><img src="http://developer.berlios.de/images/berliOS_logo.png" ALT="BerliOS" HSPACE=5 VSPACE=5 BORDER=0 height=61 width=238></a></td>
-<td WIDTH="10" BGCOLOR="#FFCC33"><img SRC="http://developer.berlios.de/images/blank.gif" ALT="" BORDER=0 height=1 width=10></td>
-<td VALIGN=middle WIDTH="99%"><b><font size="+1">BerliOS</font></b>
-<br>The Open Source Mediator</td>
-
-<!-- logo at right -->
-<td VALIGN=middle>
-<a href="http://www.fokus.fraunhofer.de/" TARGET="_blank"><img SRC="http://developer.berlios.de/images/logo_fokus.png" ALT="FOKUS" HSPACE=10 BORDER=0 height=60 width=60 align=RIGHT></a></td>
-</tr>
-<!-- end logo at right -->
-
-<tr>
-<td COLSPAN="4" BGCOLOR="#7B7B7B"><img SRC="http://developer.berlios.de/images/blank.gif" ALT="" height=2 width=2></td>
-</tr>
-</table>
-<!-- end top title -->
-
 <!-- content -->
 <p>
 <?php
@@ -51,7 +19,7 @@ if ( file_exists("/usr/local/httpd/include/adsense.php") ) {
 <tr VALIGN=TOP>
 <td align=center>
 <h2>Welcome to <?php echo "http://".$HTTP_HOST; ?></h2>
-<p>We are sorry but this project has not yet uploaded its personal webpage yet.
+<p>This is the homepage of paella.  This site is under construction
 <br>Please check back soon for updates or visit <a href="http://developer.berlios.de/">BerliOS Developer Project</a> Homepage.
 <p>The project is hosted by 
 <a href="http://developer.berlios.de">

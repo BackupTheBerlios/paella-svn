8352355308e9ec9a4e7329ccba3764350f99d0c6
umeboshi <umeboshi@cfc4e7be-4be4-0310-bcfe-fc894edce94f>
2005-08-05 18:21:44 +0000

    added notes

--- website/index.php
+++ website/index.php
@@ -9,11 +9,6 @@
 
 <!-- content -->
 <p>
-<?php
-if ( file_exists("/usr/local/httpd/include/adsense.php") ) {
-        include("/usr/local/httpd/include/adsense.php");
-}
-?>
 <p>
 <table BORDER=0 CELLSPACING=0 CELLPADDING=0 WIDTH="100%" >
 <tr VALIGN=TOP>
@@ -21,6 +16,8 @@ if ( file_exists("/usr/local/httpd/include/adsense.php") ) {
 <h2>Welcome to <?php echo "http://".$HTTP_HOST; ?></h2>
 <p>This is the homepage of paella.  This site is under construction
 <br>Please check back soon for updates or visit <a href="http://developer.berlios.de/">BerliOS Developer Project</a> Homepage.
+<br><a href:="notes.html">Notes about Paella</a>
+<br>
 <p>The project is hosted by 
 <a href="http://developer.berlios.de">
 <img src="http://developer.berlios.de/bslogo.php?group_id=0&type=1" width="124" height="32" border="0" alt="BerliOS Logo"></A>
@@ -30,16 +27,6 @@ if ( file_exists("/usr/local/httpd/include/adsense.php") ) {
 </table>
 <p>&nbsp;
 <!-- end content -->
-
-<!-- footer -->
-<table BORDER=0 CELLSPACING=0 CELLPADDING=2 WIDTH="100%" BGCOLOR="#7B7B7B" >
-<tr>
-<td ALIGN=CENTER><span class="titlebar"><font color="#FFFFFF">Copyright
-&copy; 2000-<?php echo date("Y"); ?> <a href="http://www.fokus.fraunhofer.de/" class="maintitlebar" TARGET="_blank">
-FOKUS</a></font></span></td>
-</tr>
-</table>
-<!-- end footer -->
 
 </body>
 </html>
